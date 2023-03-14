0c5b0edf9d748f425e8ed1bda122d9a40a04d8d7
humbertomottadacunha@gmail.com <humbertomottadacunha@gmail.com>
2023-03-14 15:50:46 -0300

    config

---
+++ src/routes/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home, Presentation } from "../pages";
+import { ErrorBoundary } from "./error-boundary";
+import { NotFoundPage } from "./not-found";
+
+export function MyRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" errorElement={<ErrorBoundary />}>
+          <Route index element={<Home />} />
+          <Route path="presentation" element={<Presentation />} />
+        </Route>
+        {/* <Route path="*" element={<NotFoundPage />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
